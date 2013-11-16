461ba1c6516dcf5235fcc450b14830a63ad9cb47
fabi1816 <fabi1816@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-16 22:48:29 +0000

    Se modificaron los scripts de telnet para que seteen (y borren) sus tablas de ruteo

--- trunk/ tp-distribuidos-grupo3/scripts/setupTelnetCliente.sh
+++ trunk/ tp-distribuidos-grupo3/scripts/setupTelnetCliente.sh
@@ -15,11 +15,13 @@ TAP_RED_N="tap321"
 NETMASK_N=255.255.255.128
 PORTNUM_N=32561			#Random
 IP_RED_N="10.47.1.130"
+DEFAULT_GATEWAY_N="10.47.1.129"     #R9
 
 TAP_RED_ENIE="tap654"
 NETMASK_ENIE=255.255.254.0
 PORTNUM_ENIE=32516		#Random
 IP_RED_ENIE="10.47.2.129"
+DEFAULT_GATEWAY_ENIE="10.47.2.1"    #R14
 
 #Crea las interfacez ethernet
 sudo openvpn --mktun --dev ${TAP_RED_N}
@@ -30,8 +32,11 @@ sudo ifconfig ${TAP_RED_ENIE} 0.0.0.0 promisc up
 
 #Levanta los tuneles en distintas consolas
 gnome-terminal --title=TELServer_n -x sudo openvpn --remote ${IP_SIMULACION} --port ${PORTNUM_N} --dev ${TAP_RED_N} --ifconfig ${IP_RED_N} ${NETMASK_N} 
-
 gnome-terminal --title=TELServer_enie -x sudo openvpn --remote ${IP_SIMULACION} --port ${PORTNUM_ENIE} --dev ${TAP_RED_ENIE} --ifconfig ${IP_RED_ENIE} ${NETMASK_ENIE}
+
+#Una vez que los tuneles esten corriendo se agregan las entradas en la tabla de ruteo
+sudo route add default gw ${DEFAULT_GATEWAY_N} ${TAP_RED_N}
+sudo route add default gw ${DEFAULT_GATEWAY_ENIE} ${TAP_RED_ENIE}
 
 
 
