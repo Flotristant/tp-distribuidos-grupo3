31711913df9c8be0028465f8cb9cd1066f63f3f5
fabi1816 <fabi1816@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-16 22:41:20 +0000

    En lugar de ejecutar el openVPN en segundo plano lo abro en otra ventana para tener mekor control sobre el mismo, Se agrego la limpieza de las tablas de ruteo

---  tp-distribuidos-grupo3/scripts/setupCliente.sh
+++  tp-distribuidos-grupo3/scripts/setupCliente.sh
@@ -17,41 +17,41 @@ if [ "${NOMBRE_CLIENTE}" == "HostA"  ]
 then
 	TAP="tap64"
 	HOST_IP="10.118.5.6"
-	HOST_NET="10.118.5.0"
 	NETMASK="255.255.255.0"
 	PORT_NUM="14258"
+    DEFAULT_GATEWAY="10.118.5.1"    #R1
 
 elif [ "${NOMBRE_CLIENTE}" == "HostB" ]
 then
 	TAP="tap65"
 	HOST_IP="10.19.3.35"
-	HOST_NET="10.19.3.32"
 	NETMASK="255.255.255.224"
 	PORT_NUM="14369"
+    DEFAULT_GATEWAY="10.19.3.33"    #R6
 
 elif [ "${NOMBRE_CLIENTE}" == "HostC" ]
 then
 	TAP="tap66"
 	HOST_IP="10.19.3.99"
-	HOST_NET="10.19.3.96"
 	NETMASK="255.255.255.224"
 	PORT_NUM="14147"
+    DEFAULT_GATEWAY="10.19.3.97"    #R13
 
 elif [ "${NOMBRE_CLIENTE}" == "WebServer" ]
 then
 	TAP="tap118"
 	HOST_IP="192.168.71.71"
-	HOST_NET="192.168.71.0"
 	NETMASK="255.255.255.0"
 	PORT_NUM="25369"
+    DEFAULT_GATEWAY="192.168.71.3"  #R-virtual
 
 elif [ "${NOMBRE_CLIENTE}" == "FTPServer" ]
 then
 	TAP="tap119"
 	HOST_IP="10.19.2.1"
-	HOST_NET="10.19.2.0"
 	NETMASK="255.255.255.128"
 	PORT_NUM="25147"
+    DEFAULT_GATEWAY="10.19.2.5"     #R10
 
 else
 	echo "No es uno de los servicios esperados"
@@ -62,11 +62,14 @@ fi
 sudo openvpn --mktun --dev ${TAP}
 sudo ifconfig ${TAP} 0.0.0.0 promisc up
 
-#Abre el tunel hacia una IP_DST
-sudo openvpn --remote ${IP_DST} --port ${PORT_NUM} --dev ${TAP} --ifconfig ${HOST_IP} ${NETMASK} &
+#Abre el tunel hacia una IP_DST en otra ventana
+gnome-terminal --title=${NOMBRE_CLIENTE} -x sudo openvpn --remote ${IP_DST} --port ${PORT_NUM} --dev ${TAP} --ifconfig ${HOST_IP} ${NETMASK}
 
-sudo route add -net 10.118.5.0 netmask 255.255.255.0 gw 10.118.5.1 dev ${TAP}
-sudo route add default gw 10.118.5.1 ${TAP}
+#No estoy seguro si esta entrada en la tabla es necesaria
+#sudo route add -net ${SOME_NETWORK} netmask ${NETMASK} gw ${DEFAULT_GATEWAY} dev ${TAP}
+
+#Agrega un 'default gateway' a la tabla de ruteo
+sudo route add default gw ${DEFAULT_GATEWAY} ${TAP}
 
 
 
