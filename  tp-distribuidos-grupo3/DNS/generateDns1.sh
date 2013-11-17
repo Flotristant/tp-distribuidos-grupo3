938e628449936c6442605f3cd4dc698cde471eb8
gaby.ostro@gmail.com <gaby.ostro@gmail.com@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-17 00:35:54 +0000

    scripts y config files de dns, primera version con los nombres de las redes modificados. falta actualizar show logs y reset

---
+++  tp-distribuidos-grupo3/DNS/generateDns1.sh
@@ -0,0 +1,3 @@
+python setup.py lafalda
+sudo mv db.* dns1/
+sudo mv named.conf.local dns1/
---
+++  tp-distribuidos-grupo3/DNS/reset.sh
@@ -0,0 +1,6 @@
+python setup.py carlospaz
+python setup.py calamuchita
+python setup.py lafalda
+sudo mv db.* /etc/bind/
+sudo mv named.conf.local /etc/bind/
+sudo /etc/init.d/bind9 restart
