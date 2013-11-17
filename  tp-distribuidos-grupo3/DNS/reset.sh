f5969340df113e16cad7afc3f06d8cb0311913d9
gaby.ostro@gmail.com <gaby.ostro@gmail.com@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-17 00:43:58 +0000

    actualizados scripts generadores de dns

---  tp-distribuidos-grupo3/DNS/reset.sh
+++  tp-distribuidos-grupo3/DNS/reset.sh
@@ -1,6 +1,6 @@
-python setup.py carlospaz
-python setup.py calamuchita
-python setup.py lafalda
+python setup.py riogallegos
+python setup.py rioturbio
+python setup.py elcalafate
 sudo mv db.* /etc/bind/
 sudo mv named.conf.local /etc/bind/
 sudo /etc/init.d/bind9 restart
