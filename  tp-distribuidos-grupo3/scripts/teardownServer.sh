2a01ffdc5f27410e9addcc1ddd9823c667865f80
fabi1816 <fabi1816@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-19 19:45:36 +0000

    Se arreglkaron un par de errores en las configuraciones

---  tp-distribuidos-grupo3/scripts/teardownServer.sh
+++  tp-distribuidos-grupo3/scripts/teardownServer.sh
@@ -10,6 +10,10 @@ SERVER_FTP_TAP_NUMBER="tap119"
 SERVER_TEL_TAP_NUMBER_N="tap321"
 SERVER_TEL_TAP_NUMBER_ENIE="tap654"
 
+DNS_1_TAP="tap81"
+DNS_2_TAP="tap82"
+DNS_ROOT_TAP="tap83"
+
 #Borra las interfaces
 sudo openvpn --rmtun --dev ${HOST_A_TAP_NUMBER}
 sudo openvpn --rmtun --dev ${HOST_B_TAP_NUMBER}
@@ -20,4 +24,6 @@ sudo openvpn --rmtun --dev ${SERVER_FTP_TAP_NUMBER}
 sudo openvpn --rmtun --dev ${SERVER_TEL_TAP_NUMBER_N}
 sudo openvpn --rmtun --dev ${SERVER_TEL_TAP_NUMBER_ENIE}
 
-
+sudo openvpn --rmtun --dev ${DNS_1_TAP}
+sudo openvpn --rmtun --dev ${DNS_2_TAP}
+sudo openvpn --rmtun --dev ${DNS_ROOT_TAP}
