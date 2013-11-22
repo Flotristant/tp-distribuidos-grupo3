3324e9a1569b8f90941a92ca6d2010c062fcd8ca
fabi1816 <fabi1816@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-22 20:57:10 +0000

    Unifiqué todos los teardowns y zero en un solo script

--- trunk/ tp-distribuidos-grupo3/scripts/teardown.sh
+++ trunk/ tp-distribuidos-grupo3/scripts/teardown.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#Los PID de todos los processos que se llaman "openvpn"
+PID_OPENVPN=$(ps -a | grep 'openvpn' | cut -d' ' -f2)
+
+for PID in ${PID_OPENVPN}
+do
+    sudo kill ${PID}
+done
+
+
 #Recupera todas las interfaces tap del sistema
 LISTA_TAPS=$(ifconfig -a | grep 'tap' | cut -d' ' -f1)
 
@@ -11,6 +20,16 @@ do
 	#Elimina el tunel ethernet
 	sudo openvpn --rmtun --dev ${TAP}
 done
+
+
+#Las taps que no se pudieron cerrar de la forma "normal"
+LISTA_TAPS_REMANENTE=$(ifconfig -a | grep 'tap' | cut -d' ' -f1)
+for TAP_REMANENTE in ${LISTA_TAPS_REMANENTE}
+do
+	#Elimina la interfaz virtual
+	sudo ip link delete ${TAP_REMANENTE}
+done
+
 
 exit 0
 
