9bb5451717682cb4527b7604059a3a71173e6f62
ankodelia@gmail.com <ankodelia@gmail.com@595c66f0-e8a5-fabe-f43a-404f67be8830>
2013-11-24 22:40:05 +0000

    Cambios dns_config

--- trunk/ tp-distribuidos-grupo3/DNS/dns_config.sh
+++ trunk/ tp-distribuidos-grupo3/DNS/dns_config.sh
@@ -9,28 +9,13 @@ fi
 DNS_NAME=${1} 
 if [ "$DNS_NAME" == "dns1"  ]
 then
-	TAP="tap81"
 	HOST_IP="10.118.5.7"
-	NETMASK="255.255.255.0"
-	PORT_NUM="14258"
-	DEFAULT_GATEWAY="10.118.5.1"    #R1
-
 elif [ "$DNS_NAME" == "dns2"  ]
 then
-	TAP="tap82"
 	HOST_IP="10.19.2.4"
-	NETMASK="255.255.255.128"
-	PORT_NUM="14258"
-	DEFAULT_GATEWAY="10.19.2.5"    #R10
-
 elif [ "$DNS_NAME" == "dnsroot"  ]
 then
-	TAP="tap83"
 	HOST_IP="10.47.1.131"
-	NETMASK="255.255.255.192"
-	PORT_NUM="14258"
-	DEFAULT_GATEWAY="10.47.1.129"    #R9
-
 else
 	echo "Debe ser uno de: 'dns1', 'dns2' o 'dnsroot'"
 	exit 1
@@ -69,5 +54,3 @@ echo "DNS: Importando nuestros archivos de configuracion..."
 import
 echo "DNS: Importación finalizada!"
 exit 0
-
-
